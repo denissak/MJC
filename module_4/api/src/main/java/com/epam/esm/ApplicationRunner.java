37bced8bcd66e9cac3f9afbe7005d59363c29e0c
Denis Sak <denissak9@gmail.com>
2022-03-31 10:10:19 +0300

    init

--- module_4/api/src/main/java/com/epam/esm/ApplicationRunner.java
+++ module_4/api/src/main/java/com/epam/esm/ApplicationRunner.java
@@ -7,11 +7,6 @@ import org.springframework.web.SpringServletContainerInitializer;
 @SpringBootApplication
 public class ApplicationRunner extends SpringServletContainerInitializer {
 
-    //    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(ServletInitializer.class);
-//    }
-
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
     }
