46c125acdab994492bfb113d57149497877916b3
Denis Sak <denissak9@gmail.com>
2022-04-27 16:04:37 +0300

    fix cart

--- module_4/api/src/main/java/com/epam/esm/viewController/LoginViewController.java
+++ module_4/api/src/main/java/com/epam/esm/viewController/LoginViewController.java
@@ -3,13 +3,10 @@ package com.epam.esm.viewController;
 import com.epam.esm.CertificateService;
 import com.epam.esm.TagService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,32 +17,23 @@ public class LoginViewController {
 
     @RequestMapping("/login")
     public String register(Model model) {
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("mainpage");
-
         return "login";
     }
 
     @RequestMapping("/mainpage")
     public String mainpage(Model model, @RequestParam(value = "page", defaultValue = "0", required = false) int page,
-                        @RequestParam(value = "size", defaultValue = "18", required = false) int size) {
+                           @RequestParam(value = "size", defaultValue = "18", required = false) int size) {
         model.addAttribute("tags", tagService.readAll(page, size));
         return "mainpage";
     }
 
     @RequestMapping("/cart")
-    public String order(Model model, Authentication authentication) {
-//        model.addAttribute("orders", orderList);
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("mainpage");
+    public String order(Model model) {
         return "cart";
     }
 
     @RequestMapping("/certificate")
-    public String certificate(Model model, Authentication authentication) {
-//        model.addAttribute("orders", orderList);
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("mainpage");
+    public String certificate(Model model) {
         return "certificate";
     }
 }
